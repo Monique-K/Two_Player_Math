5c1dede5ab74671033319ceb7f15022636a10b50
M-Khoury <mkhoury87@hotmail.com>
2018-07-16 18:37:11 +0000

    initial commit

---
+++ main.rb
@@ -0,0 +1,14 @@
+class Game
+  who wins (compare scores)
+  if game has been won or not
+    whose turn is it (current_player)
+
+class Player
+  individual score
+  answers to qs
+
+class Turn
+  order of moves
+  generate question
+
+
